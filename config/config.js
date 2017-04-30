09833a376fb6125398324ae7f708a15ab2c46eb5
TrickTales <wolloyo@gmail.com>
2017-04-30 17:26:31 -0400

    Update config.js

--- config/config.js
+++ config/config.js
@@ -212,8 +212,8 @@ exports.replsocketmode = 0o600;
 
 //github repo config
 exports.github = {
-	secret: "test_server", // Your repo secret
-	port: "8084", // Desired port, must be unused and above 1000
+	secret: "", // Your repo secret
+	port: "", // Desired port, must be unused and above 1000
 	rooms: ['development'], // Desired rooms
 };
 
