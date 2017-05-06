3085b3cc2bd7238750361aadd868a8a4268d7a56
TrickTales <wolloyo@gmail.com>
2017-05-06 10:21:25 -0400

    Update users.js

--- users.js
+++ users.js
@@ -46,28 +46,6 @@ function isHoster(user) {
 	if (hoster === 1) return true;
 	return false;
 }
-function generateNews () {
-			let lobby = Rooms('lobby');
-			if (!lobby) return false;
-			if (!lobby.news || Object.keys(lobby.news).length < 0) return false;
-			if (!lobby.news) lobby.news = {};
-			let news = lobby.news, newsDisplay = [];
-			Object.keys(news).forEach(announcement => {
-				newsDisplay.push(`<h4>${announcement}</h4>${news[announcement].desc}<br /><br /><strong>—<font color="${EM.Color(news[announcement].by)}">${news[announcement].by}</font></strong> on ${moment(news[announcement].posted).format("MMM D, YYYY")}`);
-			});
-			return newsDisplay;
-		}
-
-function newsDisplay(user) {
-			if (!Users(user)) return false;
-			let newsDis = generateNews();
-			if (newsDis.length === 0) return false;
-
-			if (newsDis.length > 0) {
-				newsDis = newsDis.join('<hr>');
-				return Users(user).send(`|pm| Server News|${Users(user).getIdentity()}|/raw ${newsDis}`);
-			}
-}
 
 /*********************************************************
  * Users map
