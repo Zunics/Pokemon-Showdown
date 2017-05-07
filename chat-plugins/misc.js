fd328bcda8f7c5913f162fac693261814289fd40
TrickTales <wolloyo@gmail.com>
2017-05-06 20:36:25 -0400

    Update misc.js

--- chat-plugins/misc.js
+++ chat-plugins/misc.js
@@ -292,35 +292,36 @@ staff: 'authlist',
 			return this.sendReply("/roomowner - This room isn't designed for per-room moderation to be added");
 		}
 		target = this.splitTarget(target, true);
-		var targetUser = this.targetUser;
+		let targetUser = this.targetUser;
 
 		if (!targetUser) return this.sendReply("User '" + this.targetUsername + "' is not online.");
 
 		if (!room.founder) return this.sendReply('The room needs a room founder before it can have a room owner.');
-		if (room.founder !== user.userid && !this.can('declare')) return this.sendReply('/roomowner - Access denied.');
+		if (room.founder !== user.userid && !this.can('makeroom')) return this.sendReply('/roomowner - Access denied.');
 
 		if (!room.auth) room.auth = room.chatRoomData.auth = {};
 
-		var name = targetUser.name;
+		let name = targetUser.name;
 
 		room.auth[targetUser.userid] = '&';
 		this.addModCommand("" + name + " was appointed Room Leader by " + user.name + ".");
 		room.onUpdateIdentity(targetUser);
 		Rooms.global.writeChatRoomData();
 	},
+
 	roomdeleader: 'deroomowner',
 	deroomleader: function (target, room, user) {
 		if (!room.auth) {
 			return this.sendReply("/roomdeowner - This room isn't designed for per-room moderation");
 		}
 		target = this.splitTarget(target, true);
-		var targetUser = this.targetUser;
-		var name = this.targetUsername;
-		var userid = toId(name);
+		let targetUser = this.targetUser;
+		let name = this.targetUsername;
+		let userid = toId(name);
 		if (!userid || userid === '') return this.sendReply("User '" + name + "' does not exist.");
 
-		if (room.auth[userid] !== '&') return this.sendReply("User '"+name+"' is not a room leader.");
-		if (!room.founder || user.userid !== room.founder && !this.can('declare', null, room)) return false;
+		if (room.auth[userid] !== '&') return this.sendReply("User '" + name + "' is not a room leader.");
+		if (!room.founder || user.userid !== room.founder && !this.can('makeroom', null, room)) return false;
 
 		delete room.auth[userid];
 		this.sendReply("(" + name + " is no longer Room Leader.)");
@@ -329,6 +330,7 @@ staff: 'authlist',
 			Rooms.global.writeChatRoomData();
 		}
 	},
+
 	'!errorlog': true,
 	errorlog: function (target, room, user, connection) {
 		if (!this.can('hotpatch')) return;
