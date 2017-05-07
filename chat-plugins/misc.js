474e8e644c9ff2ee0c801ab133008834c7e01276
TrickTales <wolloyo@gmail.com>
2017-05-06 20:28:49 -0400

    Update misc.js

--- chat-plugins/misc.js
+++ chat-plugins/misc.js
@@ -249,47 +249,44 @@ staff: 'authlist',
 			);
 		});
 	},
-    roomfounder: function (target, room, user) {
-        if (!room.chatRoomData) {
-            return this.sendReply("/roomfounder - This room isn\'t designed for per-room moderation to be added.");
-        }
-        target = this.splitTarget(target, true);
-        var targetUser = this.targetUser;
-        if (!targetUser) return this.sendReply("User '" + this.targetUsername + "' is not online.");
-        if (!this.can('declare')) return false;
-        if (!room.auth) room.auth = room.chatRoomData.auth = {};
-        if (!room.leagueauth) room.leagueauth = room.chatRoomData.leagueauth = {};
-        var name = targetUser.name;
-        room.auth[targetUser.userid] = '#';
-        room.founder = targetUser.userid;
-        this.addModCommand(name + ' was appointed to Room Founder by ' + user.name + '.');
-        room.onUpdateIdentity(targetUser);
-        room.chatRoomData.founder = room.founder;
-        Rooms.global.writeChatRoomData();
-    },
 
-    roomdefounder: 'deroomfounder',
-    deroomfounder: function (target, room, user) {
-        if (!room.auth) {
-            return this.sendReply("/roomdeowner - This room isn't designed for per-room moderation");
-        }
-        target = this.splitTarget(target, true);
-        var targetUser = this.targetUser;
-        var name = this.targetUsername;
-        var userid = toId(name);
-        if (!userid || userid === '') return this.sendReply("User '" + name + "' does not exist.");
+	rf: 'roomfounder',
+	roomfounder: function (target, room, user) {
+		if (!room.chatRoomData) return this.sendReply("/roomfounder - This room isn't designed for per-room moderation to be added.");
+		target = this.splitTarget(target, true);
+		let targetUser = this.targetUser;
+		if (!targetUser) return this.sendReply("User '" + this.targetUsername + "' is not online.");
+		if (!this.can('declare')) return false;
+		if (room.isPersonal) return this.sendReply("You can't do this in personal rooms.");
+		if (!room.auth) room.auth = room.chatRoomData.auth = {};
+		if (!room.leagueauth) room.leagueauth = room.chatRoomData.leagueauth = {};
+		let name = targetUser.name;
+		room.auth[targetUser.userid] = '#';
+		room.founder = targetUser.userid;
+		this.addModCommand(name + ' was appointed to Room Founder by ' + user.name + '.');
+		room.onUpdateIdentity(targetUser);
+		room.chatRoomData.founder = room.founder;
+		Rooms.global.writeChatRoomData();
+	},
 
-        if (room.auth[userid] !== '#') return this.sendReply("User '" + name + "' is not a room founder.");
-        if (!this.can('declare', null, room)) return false;
+	roomdefounder: 'deroomfounder',
+	deroomfounder: function (target, room, user) {
+		if (!room.auth) return this.sendReply("/roomdeowner - This room isn't designed for per-room moderation");
+		target = this.splitTarget(target, true);
+		let targetUser = this.targetUser;
+		let name = this.targetUsername;
+		let userid = toId(name);
+		if (room.isPersonal) return this.sendReply("You can't do this in personal rooms.");
+		if (!userid || userid === '') return this.sendReply("User '" + name + "' does not exist.");
+		if (room.auth[userid] !== '#') return this.sendReply("User '" + name + "' is not a room founder.");
+		if (!this.can('declare')) return false;
+		delete room.auth[userid];
+		delete room.founder;
+		this.sendReply(name + ' was demoted from Room Founder by ' + user.name + '.');
+		if (targetUser) targetUser.updateIdentity();
+		if (room.chatRoomData) Rooms.global.writeChatRoomData();
+	},
 
-        delete room.auth[userid];
-        delete room.founder;
-        this.sendReply("(" + name + " is no longer Room Founder.)");
-        if (targetUser) targetUser.updateIdentity();
-        if (room.chatRoomData) {
-            Rooms.global.writeChatRoomData();
-        }
-    },
 	roomleader: function (target, room, user) {
 		if (!room.chatRoomData) {
 			return this.sendReply("/roomowner - This room isn't designed for per-room moderation to be added");
