4b58d20d97bf0d79abb4688d13c3aefcb42804af
TrickTales <wolloyo@gmail.com>
2017-05-10 18:25:04 -0400

    Update customicon.js

--- chat-plugins/customicon.js
+++ chat-plugins/customicon.js
@@ -1,11 +1,21 @@
 /*
-* customicon.js
-* Credits: Lord Haji, panpawn
+- * Customicon Plugin
+- * 
 */
+
 'use strict';
 
 let icons = {};
 const fs = require('fs');
+const path = require('path');
+
+function logMoney(message) {
+       if (!message) return;
+       let file = path.join(LOGS_DIR + 'money.txt');
+       let date = '[' + new Date().toUTCString() + '] ';
+       let msg = message + '\n';
+       fs.appendFile(file, date + msg);
+}
 
 function load() {
 	fs.readFile(DATA_DIR + 'icons.json', 'utf8', function (err, file) {
@@ -17,69 +27,91 @@ load();
 
 function updateIcons() {
 	fs.writeFileSync(DATA_DIR + 'icons.json', JSON.stringify(icons));
-
-	let newCss = '/* ICONS START */\n';
+	//Marca de actualizado
+	let newCss = '\n/*. New Icons (Delete Previous) .*/';
 
 	for (let name in icons) {
 		newCss += generateCSS(name, icons[name]);
 	}
-	newCss += '/* ICONS END */\n';
+	newCss += '';
 
-	let file = fs.readFileSync(DATA_DIR + 'custom.css', 'utf8').split('\n');
-	if (~file.indexOf('/* ICONS START */')) file.splice(file.indexOf('/* ICONS START */'), (file.indexOf('/* ICONS END */') - file.indexOf('/* ICONS START */')) + 1);
-	fs.writeFileSync(DATA_DIR + 'custom.css', file.join('\n') + newCss);
-	EM.reloadCSS();
+	let file = fs.readFileSync(DATA_DIR + 'custom.css', 'utf8').split('');
+	if (~file.indexOf('')) file.splice(file.indexOf(''), (file.indexOf('') - file.indexOf('')) + 1);
+	fs.writeFileSync(DATA_DIR + 'custom.css', file.join('') + newCss);
 }
+updateIcons = updateIcons;
 
 function generateCSS(name, icon) {
 	let css = '';
 	let rooms = [];
 	name = toId(name);
 	Rooms.rooms.forEach((curRoom, id) => {
-		if (curRoom.id === 'global' || curRoom.type !== 'chat' || curRoom.isPersonal) return;
+		if (id === 'global' || curRoom.type !== 'chat' || curRoom.isPersonal) return;
 		if (!isNaN(Number(id.charAt(0)))) return;
 		rooms.push('#' + id + '-userlist-user-' + name);
 	});
 	css = rooms.join(', ');
-	css += '{\nbackground: url("' + icon + '") no-repeat right\n}\n';
+	css += '{background: url("' + icon + '") no-repeat right}';
 	return css;
 }
 
 exports.commands = {
-	uli: 'icon',
-	userlisticon: 'icon',
-	customicon: 'icon',
-	icon: {
-		set: function (target, room, user) {
-			if (!this.can('roomowner')) return false;
+	customicon: {
+	'! ':true,
+	'': function (target, room, user) {
+			if (!this.runBroadcast()) return;
+            if (!this.can('forcewin')) return false;
+			this.sendReplyBox(
+				'<small><em>(Only Administrators(~) and Leaders(&) can use these commands)</em></small><br />' +
+				'/customicon set <em>‹user›</em>, <em>‹img›</em><br />' +
+				'/customicon delete <em>‹user›</em><br />' +
+				'/customicon move <em>‹user1›</em>, <em>‹user2›</em>, <em>‹img›</em><br />' +
+				'<hr /><b>Note:</b> The image must strictly be <b>32x32</b><br>' +
+				'When moving an icon you have to put the link that the user wants whether they want the same or another icon'
+				);
+		},
+		   move: function(target, room, user, connection, cmd) {
+            if (!this.can('forcewin')) return false;
+    		if (!target || !target.trim()) return this.sendReply('/custom color move User 1, User 2, hex.');
+    		target = target.split(',');
+    		if (target.length < 3) return this.sendReply('/custom color move User 1, User 2, hex.');
+            
+            let user1 = (Users.getExact(target[0]) ? Users.getExact(target[0]).name : target[0]);
+    		let user2 = (Users.getExact(target[1]) ? Users.getExact(target[1]).name : target[1]);
+    		if (!toId(user1) || !toId(user2)) return this.sendReply('/custom color move User 1, User 2, hex.');
+	 	 Rooms.rooms.get('staff').add('|raw|<small>[CUSTOMICON] <b><font style="color:' + EM.Color(user.name) + '">' + user.name + '</font></b> has moved the customicon of ' + target[0] + ' to '  + target[1] + ' <a href="' + target[2] + '">(IMG)</a></small>');
+		   Rooms.rooms.get('staff').update();
+		   delete icons[toId(target[0])];
+    	 icons[toId(target[1])] = target[2];
+       updateIcons();
+		   },
+		
+		set: function (target, room, user, connection) {
+			if (!this.can('forcewin')) return false;
 			target = target.split(',');
-			for (let u in target) target[u] = target[u].trim();
-			if (target.length !== 2) return this.parse('/help icon');
-			if (toId(target[0]).length > 19) return this.errorReply("Usernames are not this long...");
-			if (icons[toId(target[0])]) return this.errorReply("This user already has a custom userlist icon.  Do /icon delete [user] and then set their new icon.");
-			this.sendReply("|raw|You have given " + EM.nameColor(target[0], true) + " an icon.");
-			Rooms('staff').add('|raw|' + EM.nameColor((target[0])) + ' has received an icon from ' + EM.nameColor(user.name), true).update();
-			this.privateModCommand("|raw|(" + target[0] + " has recieved icon: <img src='" + target[1] + "' width='32' height='32'> from " + user.name + ".)");
-			if (Users(target[0]) && Users(target[0]).connected) Users(target[0]).popup("|html|" + EM.nameColor(user.name, true) + " has set your userlist icon to: <img src='" + target[1] + "' width='32' height='32'><br><center>Refresh, If you don't see it.</center>");
+			
+			if (!target[1]) return this.parse('/icon');
+			if (toId(target[0]).length > 19) return this.errorReply('The username is too long.');
+			this.sendReply('|raw|The customicon of <b><font color="' + EM.Color(toId(target[1])) + '">' + Chat.escapeHTML(target[0]) + '</font></b> has been successfully established.');
+			this.privateModCommand('(' + target[0] + ' has received a customicon from ' + user.name + '.)');
+			if (Users(target) && Users(target).connected) Users(target).popup('|modal||html|' + user.name + ' has given you a customicon.');
+			logMoney(user.name + ' has established a customicon for ' + target[0] + '.');
+			Rooms.rooms.get('staff').add('|raw|<small>[ICONS] <b><font style="color:' + EM.Color(toId(user.name)) + '">' + user.name + '</font></b> has established a customicon for ' + target[0] + ' <a href="' + target[1] + '">(IMG)</a></small>');
+			Rooms.rooms.get('staff').update();
 			icons[toId(target[0])] = target[1];
 			updateIcons();
-		},
-		remove: 'delete',
+		}, 
+		
 		delete: function (target, room, user) {
-			if (!this.can('roomowner')) return false;
-			target = toId(target);
-			if (!icons[toId(target)]) return this.errorReply('/icon - ' + target + ' does not have an icon.');
+			if (!this.can('forcewin')) return false;
+			
+			if (!icons[toId(target)]) return this.errorReply('The user: ' + target + ' does not have a customicon set.');
 			delete icons[toId(target)];
 			updateIcons();
-			this.sendReply("You removed " + target + "'s icon.");
-			Rooms('staff').add('|raw|' + EM.nameColor(user.name, true) + " removed " + target + "'s icon.").update();
-			this.privateModCommand("(" + target + "'s icon was removed by " + user.name + ".)");
-			if (Users(target) && Users(target).connected) Users(target).popup("|html|" + EM.nameColor(user.name, true) + " has removed your userlist icon.");
+			this.sendReply('Has removed the customicon of ' + target + '.');
+			this.privateModCommand('(The customicon of ' + target + ' has been removed by ' + user.name + '.)');
+			logMoney(user.name + ' has removed the customicon of ' + target + '.');
+			if (Users(target) && Users(target).connected) Users(target).popup('|modal||html|' + user.name + ' has removed your customicon.');
 		},
 	},
-	iconhelp: [
-		"Commands Include:",
-		"/icon set [user], [image url] - Gives [user] an icon of [image url]",
-		"/icon delete [user] - Deletes a user's icon",
-	],
 };
