5fb6601551dc5f7604593837c9105aa19b9aeefc
TrickTales <wolloyo@gmail.com>
2017-05-10 18:47:32 -0400

    Update index.js

--- tournaments/index.js
+++ tournaments/index.js
@@ -957,10 +957,10 @@ class Tournament {
 			if (firstMoney < 2) firstMoney = 2;
 			if (Users(wid).tourBoost) firstMoney *= 2;
 			if (Users(wid).gameBoost) firstMoney *= 2;
-			let secondMoney = Math.round(firstMoney / 2);
+			let secondMoney = Math.round(firstMoney / 5);
 			if (runnerUp) {
-				if (Users(rid).tourBoost) secondMoney *= 2;
-				if (Users(rid).gameBoost) secondMoney *= 2;
+				if (Users(rid).tourBoost) secondMoney *= 5;
+				if (Users(rid).gameBoost) secondMoney *= 5;
 			}
 		
 			Economy.writeMoney(wid, firstMoney, () => {
@@ -973,7 +973,7 @@ class Tournament {
 			});
 			this.room.addRaw("<b><font color='" + color + "'>" + Chat.escapeHTML(winner) + "</font> has won " + "<font color='" + color + "'>" + firstMoney + " </font>" + (firstMoney === 1 ? global.moneyName : global.moneyPlural) + " for winning the tournament!</b>");
 
-			if ((tourSize >= 2) && this.room.isOfficial) {
+			if ((tourSize >= 3) && this.room.isOfficial) {
 				let tourRarity = EM.tourCard(tourSize, toId(winner));
 				this.room.addRaw("<b><font color='" + EM.Color(winner) + "'>" + Chat.escapeHTML(winner) + "</font> has also won a <font color=" + tourRarity[0] + ">" + tourRarity[1] + "</font> card: <button class='tourcard-btn' style='border-radius: 20px; box-shadow: 1px 1px rgba(255, 255, 255, 0.3) inset, -1px -1px rgba(0, 0, 0, 0.2) inset, 2px 2px 2px rgba(0, 0, 0, 0.5);' name='send' value='/card " + tourRarity[2] + "'>" + tourRarity[3] + "</button> from the tournament.");
 			}
